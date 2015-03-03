1254c49be8b20c757d24f7b15a90c07f1bccb272
kielejocain <joecken@gmail.com>
2015-03-02 18:04:21 -0800

    new GP cont

--- R3/pred1_GP.R
+++ R3/pred1_GP.R
@@ -1,8 +1,8 @@
 source("~/workspace/NHL_regression/R3/setup.R")
 skaterstats <- nhlClean()
-gpModel <- nhlModel(2012, 2013, 3, seed = 798619)
+gpModel <- nhlModel(2013, 2013, 3, seed = 404508)
 names(skaterstats)[gpModel[["cols"]]]
-cols <- c(1:3, 6, 15, 24, 30, 41)
+cols <- c(1:3, 6, 15, 24, 30, 38:41)
 plotData <- skaterstats[skaterstats$season %in% c(2013, 2014), c(1, 2, 3)]
 plotData <- reshape(plotData, timevar = "season", idvar = "nhl_num", direction = "wide")
 plotData <- plotData[complete.cases(plotData), ]
@@ -14,8 +14,8 @@ qplot(games_played.2014, games_played.2013, data = plotData, geom = c("smooth", 
       main = "Naive Model: Games Played, 2013 vs. 2014",
       xlab = "Games Played in 2013", ylab = "Games Played in 2014")
 dev.off()
-gpTest <- nhlModel(2009, 2010, 3, cols = cols, seed = 794825)
-corData <- nhlShape(2009, 2010, cols = cols, outcome = 3, rm.nhlnum = FALSE)
+gpTest <- nhlModel(2010, 2010, 3, cols = cols, seed = 794825)
+corData <- nhlShape(2010, 2010, cols = cols, outcome = 3, rm.nhlnum = FALSE)
 corData <- merge(corData, nhlPredict(2009, 2010, 3, gpTest))
 corData$rf[corData$rf > 1] <- 1
 corData$gbm[corData$gbm > 1] <- 1
@@ -48,8 +48,8 @@ qplot(outcome, games_played, data = corData, geom = c("smooth", "point"),
       main = "Naive Model: Games Played in 2011, Joint Model",
       xlab = "Predicted Games Played", ylab = "Actual Games Played")
 dev.off()
-testData <- nhlShape(2010, 2011, cols = cols, outcome = 3, rm.nhlnum = FALSE)
-testData <- merge(testData, nhlPredict(2010, 2011, 3, gpTest))
+testData <- nhlShape(2011, 2011, cols = cols, outcome = 3, rm.nhlnum = FALSE)
+testData <- merge(testData, nhlPredict(2011, 2011, 3, gpTest))
 testData$rf[testData$rf > 1] <- 1
 testData$gbm[testData$gbm > 1] <- 1
 testData$games_played[testData$games_played > 1] <- 1
@@ -61,8 +61,8 @@ cor(testData$outcome, testData$games_played.1)
 cor(testData$outcome, testData$rf)
 cor(testData$outcome, testData$gbm)
 cor(testData$outcome, testData$games_played)
-test2Data <- nhlShape(2011, 2012, cols = cols, outcome = 3, rm.nhlnum = FALSE)
-test2Data <- merge(test2Data, nhlPredict(2011, 2012, 3, gpTest))
+test2Data <- nhlShape(2012, 2012, cols = cols, outcome = 3, rm.nhlnum = FALSE)
+test2Data <- merge(test2Data, nhlPredict(2012, 2012, 3, gpTest))
 test2Data$rf[test2Data$rf > 1] <- 1
 test2Data$gbm[test2Data$gbm > 1] <- 1
 test2Data$games_played[test2Data$games_played > 1] <- 1
