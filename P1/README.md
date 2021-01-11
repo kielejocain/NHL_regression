7deec3dd974f0f028b30c622b7e100d973b78354
kielejocain <joecken@gmail.com>
2021-01-10 18:23:11 -0800

    Start 2020 Python version

---
+++ P1/README.md
@@ -0,0 +1,13 @@
+# Python NHL Models
+
+## Setup
+
+for Linux Mint, I needed to install the Postgres and Python development libraries before my psycopg2 Python module could install:
+
+    sudo apt-get install libpq-dev python-dev
+
+After that, I was able to install my Python requirements without issue.
+
+    pip install -r requirements.txt
+
+
---
+++ P1/clean-data.py
@@ -0,0 +1,47 @@
+# clean-data.py
+#
+# Download and prepare the data for use from a postgres instance.
+#######################################
+
+from configparser import ConfigParser
+
+import pandas as pd
+import psycopg2
+
+
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+def gather(query):
+    conn = psycopg2.connect(**config())
+    cur = conn.cursor()
+    cur.execute(query)
+    cols = [d[0] for d in cur.description]
+    data = cur.fetchall()
+    conn.close()
+
+    return pd.DataFrame(data=data, columns=cols)
+
+
+if __name__ == '__main__':
+    df = gather('select * from skater_data;')
+
+    print(df.shape)
+    print(df.head())
+
+
